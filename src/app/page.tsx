63239d068a39a75eb42f76d409eecae99c967fca
devtrills <godwinikechukwu.dev@gmail.com>
2024-01-12 01:09:07 +0100

    root page styled

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,26 +1,69 @@
 "use client";
 
-import Image from 'next/image'
-import Lottie from 'lottie-react'
+import Typewriter from 'typewriter-effect';
+import Lottie, { LottieProps } from "react-lottie-player"
+
 import animationData from "@/lottie/errorComplait.json"
 
 export default function Home() {
-  const defaultOptions = {
+  const defaultOptions: LottieProps = {
     loop: true,
-    autoplay: true,
+    play: true,
     animationData: animationData,
     rendererSettings: {
        preserveAspectRatio: "xMidYMid slice"
     }
   };
-  // @ts-ignore
-  const LottieAnimation = <Lottie height={400} width={400} className='img-fluid' />;
 
 
   return (
-    // flex min-h-screen flex-col items-center justify-between p-24
-    <main className="">
-      {LottieAnimation}      
-    </main>
+    // flex min-h-screen flex-col  justify-between p-24 
+    <main className="py-8 sm:py-4 relative h-screen min-w-screen xl:bg-green-500 overflow-hidden flex flex-col lg:flex-row">
+      <div className='text-center lg:my-[8%] lg:w-[40%]'>
+        <h1 className="inline-block font-bold text-lg lg:text-xl xl:text-2xl">Flour Mill Errror Reporting Application</h1>
+        <div className='lg:block xl:block hidden mt-8'>
+          <Typewriter
+            onInit={(typewriter) => {
+              typewriter.typeString('Hello, and Welcome!')
+                .callFunction(() => {
+                  console.log('String typed out!');
+                })
+                .pauseFor(2500)
+                .deleteAll()
+                .typeString('Sign in or Sign up to start logging errors.')
+                .pauseFor(2500)
+                .callFunction(() => {
+                  console.log('All strings were deleted');
+                })
+                .start();
+            }}
+            />
+        </div>
+      </div>
+      <div className='text-center xl:w-[40%] mt-4 sm:mt-2 lg:hidden'>
+        <Typewriter
+          onInit={(typewriter) => {
+            typewriter.typeString('Hello, and Welcome!')
+              .callFunction(() => {
+                console.log('String typed out!');
+              })
+              .pauseFor(2500)
+              .deleteAll()
+              .typeString('Sign in or Sign up to start logging errors.')
+              .pauseFor(2500)
+              .callFunction(() => {
+                console.log('All strings were deleted');
+              })
+              .start();
+          }}
+        />
+      </div>
+      <Lottie { ...defaultOptions } className="absolute top-1/2 transform -translate-y-1/2 lg:translate-x-[20%]" /> 
+      <div className='absolute w-[100%] lg:w-[40%] text-center top-[85%] transform -translate-y-[85%] sm:top-[90%] lg:top-[60%]'>
+          <button className='mx-4 bg-green-200 hover:bg-white px-4 py-2 border shadow-xl rounded-full'>Sign In</button>
+          <button className='mx-4 bg-green-500 hover:bg-white px-4 py-2 border shadow-xl rounded-full'>Sign Up</button>
+      </div>
+    </main> 
   )
 }
+//lg:-translate-x-[20%] lg:left-[15%] -translate-x-[50%] -translate-x-[50%]
